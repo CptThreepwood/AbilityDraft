49e4aac82efd0c3dd2a1bba46a295d6d1eff2128
David Jennens <d.jennens@gmail.com>
2020-07-08 01:51:29 +1000

    Parse abilities

--- src/types/Abilities.ts
+++ src/types/Abilities.ts
@@ -1,9 +1,22 @@
+import abilities from '../data/abilities.json';
+import { assert } from 'console';
+
 export function abilityUpgradeFromJson(json: any): AbilityUpgrade {
-    return {...json}
+    return {...json, ability: getAbility(json.ability)}
 }
 
 export interface AbilityUpgrade {
-    ability: number,
+    ability: Ability,
     time: number,
     level: number
+}
+
+export function getAbility(id: number): Ability {
+    const ability = abilities.filter(ability => ability.id == id);
+    assert(ability.length, `Ability ID ${id} not found`);
+    return ability[0];
+}
+
+export interface Ability {
+    name: string, id: number
 }
