3f5c1b62eb0305b14ba55f6db51f7ff1a9d58a12
David Jennens <d.jennens@gmail.com>
2020-11-08 16:56:37 +1100

    Get match by seq ID

--- src/APIRequests.ts
+++ src/APIRequests.ts
@@ -7,6 +7,7 @@ import { APIResponse_MatchSummary } from './types/APIResponses';
 
 const API_BASE = "https://api.steampowered.com/"
 const MATCH_HISTORY = "IDOTA2Match_570/GetMatchHistory/V001"
+const MATCH_SEQ_HISTORY = "IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001"
 
 export async function getMatches(): Promise<MatchSummary[]> {
     const matchesEndpoint = new URL(MATCH_HISTORY, API_BASE)
@@ -14,7 +15,24 @@ export async function getMatches(): Promise<MatchSummary[]> {
     matchesEndpoint.searchParams.append('language', steam_config.language);
     matchesEndpoint.searchParams.append('format', 'JSON');
     matchesEndpoint.searchParams.append('min_players', '10');
-    matchesEndpoint.searchParams.append('matches_requested', '500');
+    matchesEndpoint.searchParams.append('matches_requested', '100');
+    // Game Mode filtering doesn't seem to work (otherwise this method would be preferred)
+    // matchesEndpoint.searchParams.append('game_mode', '18');
+    const response = await got(matchesEndpoint.toString(), {responseType: "json"});
+    promises.writeFile('./test/rawResponse.json', JSON.stringify(response.body));
+
+    return (response.body as APIResponse_MatchSummary).result.matches.map(matchSummaryFromMatchResponse);
+}
+
+export async function getMatchesSequence(from?: number): Promise<MatchSummary[]> {
+    const matchesEndpoint = new URL(MATCH_SEQ_HISTORY, API_BASE)
+    matchesEndpoint.searchParams.append('key', steam_config.key);
+    matchesEndpoint.searchParams.append('language', steam_config.language);
+    matchesEndpoint.searchParams.append('format', 'JSON');
+    if (from) {
+        matchesEndpoint.searchParams.append('start_at_match_seq_num', `${from}`);
+    }
+    matchesEndpoint.searchParams.append('matches_requested', '100');
     
     const response = await got(matchesEndpoint.toString(), {responseType: "json"});
     promises.writeFile('./test/rawResponse.json', JSON.stringify(response.body));
