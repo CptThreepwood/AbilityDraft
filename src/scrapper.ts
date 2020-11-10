8ec346ee532b98565536234c8fe7cf53ffa6bd3b
David Jennens <d.jennens@gmail.com>
2020-11-10 17:22:42 +1100

    Unwrap recursion to explicit loop to make the logging clearer

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -15,14 +15,10 @@ async function createMatchIfMissing(match: MatchSummary): Promise<boolean> {
 
 // I think matches are created faster than we can scrape them this way
 // I'm going to run it for a while and see
-async function scrapeMatches(id: number | undefined) {
+async function scrapeMatches(id: number | undefined): Promise<number> {
     logger.info(`Getting next batch from ${id}`)
     const matches = await getMatchesSequence(id);
     const max_seq_id = Math.max(...matches.map(m => m.match_seq_num));
-    if (max_seq_id == id) {
-        logger.info('Done scrapping');
-        return;
-    }
 
     const inserts = await Promise.all(matches.map(createMatchIfMissing)).then(
         (successes) => successes.reduce((acc, cur) => acc + (cur ? 1: 0), 0)
@@ -31,8 +27,19 @@ async function scrapeMatches(id: number | undefined) {
     if (inserts < (matches.length - 1)) {
         console.warn(`Inserted ${inserts} of ${matches.length} matches`);
     }
-    await setTimeout(() => logger.info(`Sleeping for ${max_seq_id}`), 1000);
-    await scrapeMatches(max_seq_id);
+    return max_seq_id;
+}
+
+// Use an explicit loop rather than recursing
+// I'm not sure I trust the recursion in JS and this makes logging clearer
+async function scrapeMatchesController(start_id: number | undefined) {
+    let next_id = await scrapeMatches(start_id)
+    while (next_id != start_id) {
+        start_id = next_id;
+        await setTimeout(() => logger.info(`Sleeping for ${next_id}`), 1000);
+        next_id = await scrapeMatches(start_id);
+    }
+    logger.info('Done scrapping');
 }
 
 // Create Ability file if called directly
@@ -46,7 +53,7 @@ if (require.main === module) {
     ).then(
         async () => MatchSummaryModel.find().sort({match_seq_num:-1}).limit(1)
     ).then(
-        (latestMatch) => scrapeMatches(latestMatch[0]?.match_seq_num)
+        (latestMatch) => scrapeMatchesController(latestMatch[0]?.match_seq_num)
     ).then(
         () => mongoose.connection.close()
     );
