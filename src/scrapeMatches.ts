f9de3d4d128837623a9fa1b9062a93e943ac7dfd
David Jennens <d.jennens@gmail.com>
2020-11-11 17:17:41 +1100

    Get matches in batches from the DB to avoid memory issues

--- src/scrapeMatches.ts
+++ src/scrapeMatches.ts
@@ -18,21 +18,32 @@ async function createMatchIfMissing(match: Match): Promise<boolean> {
 // I think matches are created faster than we can scrape them this way
 // I'm going to run it for a while and see
 async function scrapeMatch(summary: MatchSummary): Promise<void> {
-    logger.info(`Getting match detials for ${summary._id}`)
+    logger.info(`Getting match details for ${summary._id}`)
     await getMatch(summary._id).then(createMatchIfMissing);
 }
 
 // Use an explicit loop rather than recursing
 // I'm not sure I trust the recursion in JS and this makes logging clearer
 async function scrapeMatchController() {
-    const unscrappedMatches = await MatchSummaryModel.find({'detailScrapped': false})
+    const unscrappedMatches = await MatchSummaryModel.find({
+        detailScrapped: false,
+        lobby_type: 0,
+        $where: 'this.heroes.filter(a => a.id > 0).length == 10'
+    }).limit(10);
+    if (unscrappedMatches.length == 0) {
+        logger.info('Done scrapping');
+        return;
+    }
+
     for (let match of unscrappedMatches) {
         logger.info(`Downloading match details for ${match._id}`);
         await scrapeMatch(match);
         await setTimeout(() => {}, 1000);
     }
     await Promise.all(unscrappedMatches.map(scrapeMatch))
-    logger.info('Done scrapping');
+
+    logger.info('Next Batch');
+    await scrapeMatchController();
 }
 
 // Scrape missing matches if called directly
